442fcffb18539600bb72f058a59221f423171bdd
Odeth Ayellén Peña Gómez <odethayellen@hotmail.com>
2024-05-01 19:11:12 -0600

    Agregar archivos de proyecto.

---
+++ XML/CD.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XML
+{
+    internal class CD
+    {
+        //Propierties
+
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+
+        private string artist;
+
+        public string Artist
+        {
+            get { return artist; }
+            set { artist = value; }
+        }
+
+
+        private string company;
+
+        public string Company
+        {
+            get { return company; }
+            set { company = value; }
+        }
+
+
+        private string country;
+
+        public string Country
+        {
+            get { return country; }
+            set { country = value; }
+        }
+
+
+        private double price;
+
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+
+        private int year;
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+
+        //Constructor 
+        public CD() 
+        {
+            Title = "";
+            Artist = "";
+            Country = "";
+            Company = "";
+            Price = 0;
+            Year = 0;
+        }
+
+        public CD(string title, string artist, string country,string company, double price, int year)
+        { 
+            this.title = title;
+            this.artist = artist;
+            this.country = country;
+            this.company = country;
+            this.price = price;
+            this.year = year;
+        }
+
+
+       public override string ToString() 
+        {
+            return "\nTitle:"+ title + "\nArtist:"+ artist + "\nCountry:" + artist;
+        }
+    }
+}
---
+++ XML/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection.PortableExecutable;
+using System.Xml;
+using XML;
+
+
+String URLString = "https://www.w3schools.com/xml/cd_catalog.xml";
+XmlTextReader Reader = new XmlTextReader(URLString);
+
+while (Reader.Read())
+{
+    switch (Reader.NodeType)
+    {
+        case XmlNodeType.Element: // The node is an element.
+            Console.Write("<" + Reader.Name);
+
+            while (Reader.MoveToNextAttribute()) // Read the attributes.
+                Console.Write(" " + Reader.Name + "='" + Reader.Value + "'");
+            Console.WriteLine(">");
+            break;
+        case XmlNodeType.Text: //Display the text in each element.
+            Console.WriteLine(Reader.Value);
+            break;
+        case XmlNodeType.EndElement: //Display the end of the element.
+            Console.Write("</" + Reader.Name);
+            Console.WriteLine(">");
+            break;
+    }
+}
+
+
+
+//Guardar cada elemento en un arreglo y luego imprimirlo en un nuevo fila o columna
